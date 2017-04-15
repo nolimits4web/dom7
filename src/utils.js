3dbac238c5f5065637d35212a637cdf81f10318b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-15 13:56:42 +0300

    Add extend method

--- src/utils.js
+++ src/utils.js
@@ -267,9 +267,28 @@ const Utils = {
   },
   supportTouch: !!(('ontouchstart' in window) || (window.DocumentTouch && document instanceof window.DocumentTouch)),
   removeDiacritics(str) {
-    return str.replace(/[^\u0000-\u007E]/g, function map(a) {
-      return diacriticsMap[a] || a;
-    });
+    return str.replace(/[^\u0000-\u007E]/g, a => diacriticsMap[a] || a);
+  },
+  extend(...args) {
+    const to = Object(args[0]);
+    for (let i = 1; i < args.length; i += 1) {
+      const nextSource = args[i];
+      if (nextSource !== undefined && nextSource !== null) {
+        const keysArray = Object.keys(Object(nextSource));
+        for (let nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex += 1) {
+          const nextKey = keysArray[nextIndex];
+          const desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+          if (desc !== undefined && desc.enumerable) {
+            if (typeof to[nextKey] === 'object' && typeof nextSource[nextKey] === 'object') {
+              Utils.extend(to[nextKey], nextSource[nextKey]);
+            } else {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+    }
+    return to;
   },
 };
 
