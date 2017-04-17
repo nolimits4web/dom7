e6df4d67126202091a5fae20f4d09b722c983444
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-04-17 12:39:52 +0300

    Fix empty undefined object in param

--- src/utils.js
+++ src/utils.js
@@ -108,12 +108,12 @@ const Utils = {
     let length;
     if (typeof urlToParse === 'string' && urlToParse.length) {
       urlToParse = urlToParse.indexOf('?') > -1 ? urlToParse.replace(/\S*\?/, '') : '';
-      params = urlToParse.split('&');
+      params = urlToParse.split('&').filter(paramsPart => paramsPart !== '');
       length = params.length;
 
       for (i = 0; i < length; i += 1) {
         param = params[i].replace(/#\S+/g, '').split('=');
-        query[decodeURIComponent(param[0])] = decodeURIComponent(param[1]) || '';
+        query[decodeURIComponent(param[0])] = typeof param[1] === 'undefined' ? undefined : decodeURIComponent(param[1]) || '';
       }
     }
     return query;
