2758ea0ddf67f14b5308c77e3bdcdeee27f9796d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-09-02 14:42:54 +0300

    Remove get translate util

--- src/utils.js
+++ src/utils.js
@@ -115,44 +115,6 @@ function toCamelCase(string) {
 function dataset(el) {
   return $(el).dataset();
 }
-function getTranslate(el, axis = 'x') {
-  const curStyle = window.getComputedStyle(el, null);
-  let matrix;
-  let curTransform;
-  let transformMatrix;
-
-  if (window.WebKitCSSMatrix) {
-    curTransform = curStyle.transform || curStyle.webkitTransform;
-    if (curTransform.split(',').length > 6) {
-      curTransform = curTransform.split(', ').map(a => a.replace(',', '.')).join(', ');
-    }
-    // Some old versions of Webkit choke when 'none' is passed; pass
-    // empty string instead in this case
-    transformMatrix = new window.WebKitCSSMatrix(curTransform === 'none' ? '' : curTransform);
-  } else {
-    transformMatrix = curStyle.transform || curStyle.getPropertyValue('transform').replace('translate(', 'matrix(1, 0, 0, 1,');
-    matrix = transformMatrix.toString().split(',');
-  }
-
-  if (axis === 'x') {
-    // Latest Chrome and webkits Fix
-    if (window.WebKitCSSMatrix) curTransform = transformMatrix.m41;
-    // Crazy IE10 Matrix
-    else if (matrix.length === 16) curTransform = parseFloat(matrix[12]);
-    // Normal Browsers
-    else curTransform = parseFloat(matrix[4]);
-  }
-  if (axis === 'y') {
-    // Latest Chrome and webkits Fix
-    if (window.WebKitCSSMatrix) curTransform = transformMatrix.m42;
-    // Crazy IE10 Matrix
-    else if (matrix.length === 16) curTransform = parseFloat(matrix[13]);
-    // Normal Browsers
-    else curTransform = parseFloat(matrix[5]);
-  }
-
-  return curTransform || 0;
-}
 function requestAnimationFrame(callback) {
   if (window.requestAnimationFrame) return window.requestAnimationFrame(callback);
   else if (window.webkitRequestAnimationFrame) return window.webkitRequestAnimationFrame(callback);
@@ -195,7 +157,6 @@ const Utils = {
   param: serializeObject,
   toCamelCase,
   dataset,
-  getTranslate,
   requestAnimationFrame,
   cancelAnimationFrame,
   extend,
@@ -209,7 +170,6 @@ export {
   serializeObject,
   toCamelCase,
   dataset,
-  getTranslate,
   requestAnimationFrame,
   cancelAnimationFrame,
   extend,
