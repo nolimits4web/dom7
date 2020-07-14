e3d0732e1de6d599061e12225ad39c7d46a59540
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-07-14 18:43:56 +0300

    Get/set __proto__ correctly

--- src/dom7-class.js
+++ src/dom7-class.js
@@ -1,6 +1,20 @@
+/* eslint-disable no-proto */
+function makeReactive(obj) {
+  const proto = obj.__proto__;
+  Object.defineProperty(obj, '__proto__', {
+    get() {
+      return proto;
+    },
+    set(value) {
+      proto.__proto__ = value;
+    },
+  });
+}
 class Dom7 extends Array {
   constructor(items) {
     super(...items);
+
+    makeReactive(this);
   }
 }
 export default Dom7;
